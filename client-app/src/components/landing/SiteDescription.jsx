6550bcbbad3f1fd867776c50f303d0d788ff4d5f
Daniel Agg <daniel.agg@outlook.com>
2019-08-03 21:23:53 +0200

    Preparing for multi language support

--- client-app/src/components/landing/SiteDescription.jsx
+++ client-app/src/components/landing/SiteDescription.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import LocalizedStrings from "react-localization";
 
 const SiteDescription = () => {
+  let strings = new LocalizedStrings({
+    en: {
+      heading: "Got a gig but no gear? Want to try it out before buying?",
+      descriptionLine1:
+        "With Kalodile, you can rent any equipment, or offer your own.",
+      descriptionLine2: "You specify when and for how much."
+    },
+    hu: {
+      heading:
+        "Este koncert de a delay megdöglött? Ki akarsz egy pedált próbálni, mielőtt megveszed?",
+      descriptionLine1:
+        "A Kalodile-n keresztül kölcsönözhetsz más zenészektől, vagy te is feltöltheted kölcsönzésre cuccaid.",
+      descriptionLine2: "Te mondod meg mikor, mennyiért."
+    }
+  });
+
+  // todo, obviously
+  strings.setLanguage("hu");
+
   return (
     <div className="mt-4 p-6 text-center">
       <h1 className="font-display font-bold text-4xl">Kalodile</h1>
-      <h2 className="mt-6 font-display text-xl">
-        Got a gig but no gear? Want to try it out before buying?
-      </h2>
-      <p className="mt-4 font-display">
-        With Kalodile, you can rent any equipment, or offer your own.
-      </p>
-      <p className="font-display">You specify when and for how much.</p>
+      <h2 className="mt-6 font-display text-xl">{strings.heading}</h2>
+      <p className="mt-4 font-display">{strings.descriptionLine1}</p>
+      <p className="font-display">{strings.descriptionLine2}</p>
     </div>
   );
 };
