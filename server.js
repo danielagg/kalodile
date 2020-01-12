f00c0db2dfb4ef4b615ff857d8367610603459f6
Daniel Agg <daniel.agg@outlook.com>
2020-01-12 13:26:36 +0100

    Added Vue

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const PORT = process.env.PORT || 5000;
 app.use(require("./routes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 }
 
